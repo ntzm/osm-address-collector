35bbd9e76803c5ca797de4b61bbe49649bd3796f
Nat Zimmermann <nathanielzimmermann@gmail.com>
2023-02-04 00:47:06 +0000

    cap very inaccurate gps

--- find-nearest-streets.mjs
+++ find-nearest-streets.mjs
@@ -8,7 +8,7 @@ import overpassQuery from './overpass.mjs'
  */
 export default async function findNearestStreets(position, searchMetres, endpoint, timeout) {
   const query = `[out:json]\
-[bbox:${createBoundingBox(position, searchMetres + position.accuracy).join(',')}];\
+[bbox:${createBoundingBox(position, searchMetres + Math.max(position.accuracy, 20)).join(',')}];\
 way[highway][name];\
 for(t["name"])\
 (make x name=_.val;out;);`
