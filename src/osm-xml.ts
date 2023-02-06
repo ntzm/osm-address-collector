1d317e8f4c2196e54a5aa8deb9fed43b570cfba7
Nat Zimmermann <nathanielzimmermann@gmail.com>
2023-02-06 20:56:15 +0000

    refactor

--- src/osm-xml.ts
+++ src/osm-xml.ts
@@ -12,63 +12,66 @@ export function getOsmFile(
   osm.setAttribute('version', '0.6')
   osm.setAttribute('generator', 'OSM Address Collector')
 
-  for (const [i, address] of addresses.entries()) {
-    const node = xml.createElement('node')
-    node.setAttribute('id', String(-date.getTime() - i))
-    node.setAttribute('version', '1')
-    node.setAttribute('lat', String(address.latitude))
-    node.setAttribute('lon', String(address.longitude))
+  osm.append(
+    ...addresses.map((address, i) => {
+      const node = xml.createElement('node')
+      node.setAttribute('id', String(-date.getTime() - i))
+      node.setAttribute('version', '1')
+      node.setAttribute('lat', String(address.latitude))
+      node.setAttribute('lon', String(address.longitude))
+  
+      const tag = xml.createElement('tag')
+  
+      if (Number.isNaN(Number(address.numberOrName.charAt(0)))) {
+        tag.setAttribute('k', 'addr:housename')
+      } else {
+        tag.setAttribute('k', 'addr:housenumber')
+      }
+  
+      tag.setAttribute('v', address.numberOrName)
+  
+      node.append(tag)
+  
+      if (address.street) {
+        const streetTag = xml.createElement('tag')
+        streetTag.setAttribute('k', 'addr:street')
+        streetTag.setAttribute('v', address.street)
+  
+        node.append(streetTag)
+      }
+  
+      node.append(
+        ...address.customTags.map(({ key, value }) => {
+          if (value === '') {
+            return ''
+          }
+  
+          const customTag = xml.createElement('tag')
+          customTag.setAttribute('k', key)
+          customTag.setAttribute('v', value)
+  
+          return customTag
+        })
+      )
 
-    const tag = xml.createElement('tag')
+      return node
+    }),
+    ...notes.map((note, i) => {
+      const node = xml.createElement('node')
+      node.setAttribute('id', String(-date.getTime() - addresses.length - i))
+      node.setAttribute('version', '1')
+      node.setAttribute('lat', String(note.latitude))
+      node.setAttribute('lon', String(note.longitude))
 
-    if (Number.isNaN(Number(address.numberOrName.charAt(0)))) {
-      tag.setAttribute('k', 'addr:housename')
-    } else {
-      tag.setAttribute('k', 'addr:housenumber')
-    }
+      const tag = xml.createElement('tag')
+      tag.setAttribute('k', 'note')
+      tag.setAttribute('v', note.content)
 
-    tag.setAttribute('v', address.numberOrName)
+      node.append(tag)
 
-    node.append(tag)
-
-    if (address.street) {
-      const streetTag = xml.createElement('tag')
-      streetTag.setAttribute('k', 'addr:street')
-      streetTag.setAttribute('v', address.street)
-
-      node.append(streetTag)
-    }
-
-    node.append(
-      ...address.customTags.map(({ key, value }) => {
-        if (value === '') {
-          return ''
-        }
-
-        const customTag = xml.createElement('tag')
-        customTag.setAttribute('k', key)
-        customTag.setAttribute('v', value)
-        return customTag
-      })
-    )
-
-    osm.append(node)
-  }
-
-  for (const [i, note] of notes.entries()) {
-    const node = xml.createElement('node')
-    node.setAttribute('id', String(-date.getTime() - addresses.length - i))
-    node.setAttribute('version', '1')
-    node.setAttribute('lat', String(note.latitude))
-    node.setAttribute('lon', String(note.longitude))
-
-    const tag = xml.createElement('tag')
-    tag.setAttribute('k', 'note')
-    tag.setAttribute('v', note.content)
-
-    node.append(tag)
-    osm.append(node)
-  }
+      return node
+    }),
+  )
 
   xml.append(osm)
 
