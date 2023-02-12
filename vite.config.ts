4dba8a026f6697ad7a1b69c2c2a0e914d9772222
Nat Zimmermann <nathanielzimmermann@gmail.com>
2023-02-12 20:51:00 +0000

    reduce bundle a bit

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import react from '@vitejs/plugin-react'
-import analyze from 'rollup-plugin-analyzer'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/osm-address-collector',
   plugins: [basicSsl(), react()],
-  build: {
-    rollupOptions: {
-      plugins: [analyze()],
-    },
+  resolve: {
+    alias: [
+      {
+        find: 'leaflet/dist/leaflet.css',
+        replacement: 'leaflet/dist/leaflet.css',
+      },
+      {
+        find: 'leaflet',
+        replacement: 'leaflet/dist/leaflet-src.esm.js',
+      },
+    ],
   },
 })
