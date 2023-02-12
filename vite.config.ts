dd4d2a34a2883b99b4490b6e1aa9eacc7f67c739
Nat Zimmermann <nathanielzimmermann@gmail.com>
2023-02-12 20:01:10 +0000

    analyse bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import react from '@vitejs/plugin-react'
+import analyze from 'rollup-plugin-analyzer'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/osm-address-collector',
   plugins: [basicSsl(), react()],
+  build: {
+    rollupOptions: {
+      plugins: [analyze()],
+    },
+  },
 })
